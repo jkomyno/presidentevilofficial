f4b3baac94c2bc1ba0a6bc0b05980c56a5000a97
jkomyno <skiabo97@gmail.com>
2016-10-27 18:33:40 +0200

    first commit

---
+++ plugins/config.php
@@ -0,0 +1,38 @@
+<?php
+
+/* Contact Form
+ ---------------------------------------------------------------------- */
+ 
+// Enter your email address
+$your_email_address = 'qualsiasinomenonsiagiainuso@gmail.com';
+
+
+/* Twitter Feed
+ ---------------------------------------------------------------------- */
+
+// Twitter account name
+$username = '_President_Evil';
+
+// API key
+$consumer_key = 'Avlpk6lPu6xCUEYkg2vZJqglJ';
+
+// API secret
+$consumer_secret = 'NfDBOmKfaOUaZqp1prpYjjKgQQuRAJUG5Z7auEWNH3KG6pXt5L';
+
+// Access token
+$access_token = '2883942154-9pKZElR2sv0QvllGlyb1PCoZi9gmPCFC6DlulFj';
+
+// Access token secret
+$access_token_secret = 'JQN0fAqCnoJ7RfijAtQhcZliVBojpaqptHaRLRNqENYCm';
+
+// Do you want to show replies
+$ignore_replies = true;
+
+// Human time e.g.: "about 1 hour ago"
+$twitter_style_dates = true;
+
+// Date setting if $twitter_style_dates is false
+$date_format = 'F d, Y';
+
+// Twitter cache time (default is 1 hour)
+$twitter_cachetime = 60*60;
